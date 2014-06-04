8129f44711c2021595624c4a23dc05b1135d6fa5
nezz <imtaehyun@me.com>
2014-06-04 10:07:36 +0900

    내역 수정 삭제 function 수정

--- public/js/app.js
+++ public/js/app.js
@@ -1,4 +1,6 @@
 var app = angular.module('donmoney-web',['ngRoute']);
+//var apiUrl = 'http://localhost:8080';
+var apiUrl = 'http://nezz.pe.kr:8080';
 
 app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpProvider) {
     $routeProvider.
@@ -13,31 +15,38 @@ app.config(['$routeProvider', '$httpProvider', function($routeProvider, $httpPro
         otherwise({
             redirectTo: '/'
         });
-    //delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    $httpProvider.defaults.useXDomain = true;
+    delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
 
 app.controller('ListController', ['$http', '$scope', function($http, $scope) {
     $scope.transactions = [];
-    $http.get('http://nezz.pe.kr:8080/list').success(function(data) {
+
+    $http.get(apiUrl + '/list').success(function(data) {
         $scope.transactions = data;
     });
 }]);
 
 app.controller('DetailController', function($http, $scope, $routeParams, $location) {
     $scope.t = {};
-    $http.get('http://nezz.pe.kr:8080/get?id=' + $routeParams.id).success(function(data) {
+
+    $http.get(apiUrl + '/get?id=' + $routeParams.id).success(function(data) {
         $scope.t = data;
     });
+
     $scope.editTransaction = function() {
-        $http.post('http://nezz.pe.kr:8080/modify', $scope.t).success(function(data) {
-            alert(data);
-        });
+        $http({method: 'POST', url: apiUrl + '/modify', params: $scope.t})
+            .success(function(result) {
+                if (result === '1') $location.path('/');
+                else alert('수정 중 오류가 발생했습니다.');
+            });
     };
 
     $scope.deleteTransaction = function() {
-        $http.post('http://nezz.pe.kr:8080/delete?id='+$scope.t.id).success(function(data) {
-            alert(data);
-            $location.path('/');
-        });
+        $http.post(apiUrl + '/delete?id='+$scope.t.id)
+            .success(function(result) {
+                if (result === '1') $location.path('/');
+                else alert('삭제하지 못했습니다.');
+            });
     };
 });
